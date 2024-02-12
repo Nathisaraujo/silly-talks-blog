60e1f019451a51ac3db94d25d896a730a673d7e5
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-02-12 17:52:46 +0000

    changed allowed hosts for deployment

--- sillytalks/settings.py
+++ sillytalks/settings.py
@@ -28,11 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
-                 'https://sillytalks-blog-abbefcc01054.herokuapp.com/'
-                 'http://localhost:24000/'
+                 'https://sillytalks-blog-abbefcc01054.herokuapp.com/',
+                 '198.211.99.20', 
+                 'localhost', 
+                 '127.0.0.1',
                  ]
 
 
@@ -93,6 +95,11 @@ WSGI_APPLICATION = 'sillytalks.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
